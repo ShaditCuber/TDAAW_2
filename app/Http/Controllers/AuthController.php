bbfe3d8a6e260d6be59553e19a4f6da8e76af218
ShaditCuber <felipebastidas2013@gmail.com>
2023-12-06 12:31:22 -0300

    cambio de codigo de respuesta credenciales incorrectas

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -57,7 +57,7 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password');
         try {
             if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'invalid_credentials'], 400);
+                return response()->json(['error' => 'Credenciales Incorrectas'], 200);
             }
         } catch (JWTException $e) {
             return response()->json(['error' => 'could_not_create_token'], 500);
